4c15e9b1a0b2a9e29c7163616c8f2ec6644befb1
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2025-05-12 17:42:27 -0300

    Calculo corrigido

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 let intervalSeconds;
+var texto = document.querySelector(".timeTogether");
 
 function calculateTimeTogether() {
   clearInterval(intervalSeconds);
 
-  var texto = document.querySelector(".timeTogether");
   const day = document.querySelector("#day").value;
   const hour = document.querySelector("#hour").value;
 
-  const dateSelected = `${day}T${hour}`;
-  const date = new Date(dateSelected);
+  if(!day || !hour){
+    texto.innerHTML = "Por afvor, selecione a data e a hora";
+    return;
+  }
+
+  const dateInput = `${day}T${hour}`;
+  const dateSelected = new Date(dateInput);
+
   intervalSeconds = setInterval(() => {
     const now = new Date();
-    const result = differenceBetweenDates(dateSelected, now)
+    const result = differenceBetweenDates(dateSelected, now);
 
-    const difference = date - now;
-    const differenceSeconds = Math.floor(difference / 1000);
-    const seconds = Math.abs(differenceSeconds % 60);
-    const minutes = Math.abs(Math.floor((differenceSeconds / 60) % 60));
-    const hours = Math.abs(Math.floor((differenceSeconds / 3600) % 24));
-
-    texto.innerHTML = `${result.years} anos, ${result.months} meses, ${result.days} dias, ${hours} horas, ${minutes} minutos, ${seconds} segundos`;
+    texto.innerHTML = `${result.years} anos, ${result.months} meses, ${result.days} dias, ${result.hours} horas, ${result.minutes} minutos, ${result.seconds} segundos`;
   }, 1000);
 }
 
 function differenceBetweenDates(date1, date2){
   let d1 = new Date(date1);
   let d2 = new Date(date2);
-  console.log(d1, d2);
+  let future = false;
 
   if(d1 > d2){
     [d1,d2] = [d2,d1];
+    future = true;
   }
 
   let years = d2.getFullYear() - d1.getFullYear();
   let months = d2.getMonth() - d1.getMonth();
   let days = d2.getDate() - d1.getDate();
-  console.log(years, months, days);
 
   if(days < 0){
     months -= 1;
@@ -48,7 +48,13 @@ function differenceBetweenDates(date1, date2){
     months += 12;
   }
 
-  return {years, months, days};
+  const diffMs = Math.abs(d1 - d2);
+  const totalSeconds = Math.floor(diffMs / 1000);
+  const seconds = totalSeconds % 60;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+
+  return {years, months, days, hours, minutes, seconds};
 }
 
 
