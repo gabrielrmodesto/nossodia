2f67e13cab45b81d7feb8c1e7befe66520269e3d
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2025-06-11 00:13:38 -0300

    Slider de imagens

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function calculateTimeTogether() {
   const day = '2024-07-26';
   const hour = '22:15';
 
-  if(!day || !hour){
+  if (!day || !hour) {
     texto.innerHTML = "Por afvor, selecione a data e a hora";
     return;
   }
@@ -22,23 +22,23 @@ function calculateTimeTogether() {
     let textMonths = 'mês';
     let textDays = 'dia';
 
-    if(result.years > 1) textYears = 'anos';
+    if (result.years > 1) textYears = 'anos';
 
-    if(result.months > 1) textMonths = 'meses';
-    
-    if(result.days > 1) textDays = 'dias';
+    if (result.months > 1) textMonths = 'meses';
+
+    if (result.days > 1) textDays = 'dias';
 
     texto.innerHTML = `${result.years} ${textYears}, ${result.months} ${textMonths}, ${result.days} ${textDays}, ${result.hours} horas, ${result.minutes} minutos, ${result.seconds} segundos`;
   }, 1000);
 }
 
-function differenceBetweenDates(date1, date2){
+function differenceBetweenDates(date1, date2) {
   let d1 = new Date(date1);
   let d2 = new Date(date2);
   let future = false;
 
-  if(d1 > d2){
-    [d1,d2] = [d2,d1];
+  if (d1 > d2) {
+    [d1, d2] = [d2, d1];
     future = true;
   }
 
@@ -46,13 +46,13 @@ function differenceBetweenDates(date1, date2){
   let months = d2.getMonth() - d1.getMonth();
   let days = d2.getDate() - d1.getDate();
 
-  if(days < 0){
+  if (days < 0) {
     months -= 1;
     const lastDayOfTheMonth = new Date(d2.getFullYear(), d2.getMonth(), 0).getDate();
     days += lastDayOfTheMonth;
   }
 
-  if(months < 0){
+  if (months < 0) {
     years -= 1;
     months += 12;
   }
@@ -63,11 +63,27 @@ function differenceBetweenDates(date1, date2){
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const hours = Math.floor(totalSeconds / 3600) % 24;
 
-  return {years, months, days, hours, minutes, seconds};
+  return { years, months, days, hours, minutes, seconds };
 }
 
-function loadNossaHistoria(){
+function loadNossaHistoria() {
   window.location.href = 'home.html';
 }
 
+function sliderPhotos() {
+  let next = document.querySelector('.next');
+  let prev = document.querySelector('.prev');
+  let slider = document.querySelector('.slider');
+
+  next.addEventListener('click', function () {
+    let slides = document.querySelectorAll('.slides');
+    slider.appendChild(slides[0]);
+  })
+  prev.addEventListener('click', function () {
+    let slides = document.querySelectorAll('.slides');
+    slider.prepend(slides[slides.length - 1]);
+  })
+}
+
+sliderPhotos();
 calculateTimeTogether();
